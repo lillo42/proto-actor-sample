c456d3203ac7e02e73a42ebf1b081cd1a832f304
Actor Dev <rlillo@ActorDev.>
2025-03-04 22:32:17 +0000

    Add hello world

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,42 @@
+﻿using Proto;
+
+var system = new ActorSystem();
+var props = Props.FromProducer(() => new GreetingdActor());
+var greeter = system.Root.Spawn(props);
+
+while (true)
+{
+    Console.Write("What is your name(q to quit)? ");
+    var name = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(name))
+    {
+        continue;
+    }
+
+    if (name == "q")
+    {
+        break;
+    }
+
+
+    system.Root.Send(greeter, new Hello(name));
+
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+
+public record Hello(string Who);
+
+public class GreetingdActor : IActor
+{
+    public Task ReceiveAsync(IContext context)
+    {
+        if (context.Message is Hello hello)
+        {
+            Console.WriteLine("Hello {0}", hello.Who);
+        }
+
+        return Task.CompletedTask;
+    }
+}
